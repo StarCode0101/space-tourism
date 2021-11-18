a27ad0747f4d8eeca3ac28b02d4ddbb1cc139d5b
Bakare Abiola <murphybiola007@gmail.com>
2021-11-18 04:28:25 -0800

    Revert "updated"
    This reverts commit 5b95bc6e8af9bc63317f958a492f859edd747774.
    
    Reverted

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <nav class="absolute top-0 items-center w-full min-h-6 px-5 py-4 z-50">
+    <div class="absolute top-0 items-center w-full min-h-6 z-50 md:mb-6 lg:pt-7">
       <!--  Icon  -->
-      <div class="flex flex-col max-w-screen-xl mx-auto">
+      <div
+        class="flex flex-col md:justify-between md:flex-row max-w-screen-2xl mx-auto px-5 py-4 md:pt-0 md:pr-0 lg:pl-0"
+      >
         <!--  Mobile -->
-        <div class="flex items-center justify-between">
-          <div class="flex-1 md:flex-none lg:w-1/12 lg:pl-10">
+        <div
+          class="flex items-center justify-between md:flex-1 lg:mr-7 lg:flex-none md:pl-4 lg:pl-0"
+        >
+          <div class="md:flex-none lg:pl-10">
             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
               <g fill="none" fill-rule="evenodd">
                 <circle cx="24" cy="24" r="24" fill="#FFF" />
@@ -16,7 +20,7 @@
               </g>
             </svg>
           </div>
-          <div class="flex-none md:hidden">
+          <div class="md:hidden" @click="isHidden = !isHidden">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
               <g fill="#D0D6F9" fill-rule="evenodd">
                 <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
@@ -26,34 +30,42 @@
         </div>
         <!-- End of Mobile -->
         <!--  Nav Column -->
-        <div
-          class="flex flex-col flex-grow w-11/12 pl-5 md:flex flex-nowrap items-center"
-        >
-          <hr class="border-t-2 border-gray-600 w-2/5" />
-          <hr class="border-t-2 border-gray-600 w-12 -mr-5 z-50" />
-          <div
-            class="w-3/5 backdrop-filter backdrop-blur-lg backdrop-brightness-125 flex items-center"
+        <div class="flex-grow md:flex-grow-0 md:pl-5 items-center lg:flex-1 lg:flex">
+          <hr class="hidden lg:flex border-t-2 border-gray-600 lg:flex-1" />
+          <hr class="hidden lg:flex border-t-2 border-gray-600 w-12 -mr-8 z-50" />
+          <nav
+            :class="{ hidden: isHidden }"
+            class="flex-col md:px-12 md:flex md:flex-row mt-2 md:mt-0 text-black md:text-white bg-white md:bg-transparent md:bg-gray-800 md:bg-opacity-95 lg:justify-end xl:pr-96 lg:pr-32 lg:bg-transparent lg:backdrop-filter lg:backdrop-blur-xl lg:backdrop-brightness-125"
           >
-            <ul class="flex flex-row list-none space-x-8 pl-8">
-              <li class="font-semibold text-white">
-                <nuxt-link
-                  class="flex items-center min-h-16 hover:border-b-2 hover:border-white"
-                  to="/"
-                  >00 HOME</nuxt-link
-                >
-              </li>
-              <li class="font-semibold text-white">
-                <nuxt-link
-                  class="flex items-center min-h-16 hover:border-b-2 hover:border-white"
-                  to="/destination"
-                  >01 DESTINATION</nuxt-link
-                >
-              </li>
-            </ul>
-          </div>
+            <nuxt-link
+              class="flex items-center font-semibold py-2 md:py-9 mt-2 text-sm bg-transparent md:min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white md:active:border-white"
+              to="/"
+              >HOME</nuxt-link
+            >
+            <nuxt-link
+              class="flex items-center font-semibold py-2 md:py-9 mt-2 text-sm bg-transparent md:min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white md:active:border-white"
+              to="/destination"
+              >DESTINATIONS</nuxt-link
+            >
+            <nuxt-link
+              class="flex items-center font-semibold py-2 md:py-9 mt-2 text-sm bg-transparent md:min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white"
+              to="/"
+              >DESTINATIONS</nuxt-link
+            >
+            <nuxt-link
+              class="flex items-center font-semibold py-2 md:py-9 mt-2 text-sm bg-transparent md:min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white"
+              to="/"
+              >HOME</nuxt-link
+            >
+            <nuxt-link
+              class="flex items-center font-semibold py-2 md:py-9 mt-2 text-sm bg-transparent md:min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white"
+              to="/"
+              >HOME</nuxt-link
+            >
+          </nav>
         </div>
       </div>
-    </nav>
+    </div>
 
     <main>
       <Nuxt />
@@ -66,10 +78,13 @@ export default {
   data() {
     return {
       data: "from default",
-      screenType: "",
+      isHidden: true,
     };
   },
 
+  methods: {
+    toggleMenu() {},
+  },
   watch: {
     screenType() {
       this.OnWindowResize();
@@ -79,11 +94,11 @@ export default {
     OnWindowResize() {
       console.log(window.screen.width);
       if (window.screen.width < 760) {
-        this.screenType = "mobile";
-      } else if (screen.width > 760 && screen.width < 1280) {
-        this.screenType = "tablet";
+        this.$store.commit("changeDeviceType", "mobile");
+      } else if (screen.width > 760 && screen.width < 1024) {
+        this.$store.commit("changeDeviceType", "tablet");
       } else {
-        this.screenType = "desktop";
+        this.$store.commit("changeDeviceType", "desktop");
       }
     },
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,30 @@
 <template>
   <div>
     <div
-      class="h-full w-full z-10 bg-cover bg-center"
+      class="h-full w-full z-10 bg-cover bg-center bg-no-repeat"
       :style="{ backgroundImage: 'url(' + require(`~/assets/img/${bgImage}`) + ')' }"
     >
-      <div class="h-52 w-54 text-white pt-20"></div>
-
-      <div class="container mx-0 px-7 py-6 w-full pb-56 flex flex-wrap">
-        <div class="w-1/12"></div>
-        <div class="w-11/12 flex flex-wrap lg:flex-nowrap items-center">
+      <div
+        class="max-w-screen-2xl mx-auto md:px-32 px-7 py-6 w-full pt-28 md:pt-52 flex flex-wrap"
+      >
+        <div class="flex flex-wrap lg:flex-nowrap items-center">
           <div class="flex flex-wrap w-full">
-            <div class="w-1/2 text-white">
-              <p class="text-xl pb-2">So, you want to travel to</p>
-              <p class="text-9xl uppercase pb-8">Space</p>
-              <p class="text-lg">
+            <div class="lg:w-1/2 text-white text-center">
+              <p class="text-xl pb-2 uppercase lg:text-left">So, you want to travel to</p>
+              <p class="text-6xl md:text-9xl xl:text-12 uppercase pb-8 lg:text-left">
+                Space
+              </p>
+              <p class="text-lg lg:text-left">
                 Let’s face it; if you want to go to space, you might as well genuinely go
                 to outer space and not hover kind of on the edge of it. Well sit back, and
                 relax because we’ll give you a truly out of this world experience!
               </p>
             </div>
-            <div class="hidden w-1/2 pl-96 pt-20">
+            <div
+              class="flex justify-center lg:justify-end lg:w-1/2 pt-20 md:pt-36 md:pb-16 w-full"
+            >
               <div
-                class="flex items-center justify-center w-56 h-56 rounded-full bg-white uppercase"
+                class="bg-white flex h-32 md:h-56 w-32 md:w-56 md:text-3xl items-center justify-center rounded-full uppercase"
               >
                 Explore
               </div>
@@ -38,8 +41,17 @@ import iData from "~/content/data.json";
 export default {
   data() {
     return {
-      bgImage: "background-home-desktop.jpg",
+      // bgImage: "",
     };
+  },
+
+  computed: {
+    bgImage() {
+      if (this.$store.state.deviceType === "mobile") return "background-home-mobile.jpg";
+      else if (this.$store.state.deviceType === "tablet")
+        return "background-home-tablet.jpg";
+      else return "background-home-desktop.jpg";
+    },
   },
 };
 </script>
