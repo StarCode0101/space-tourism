c45da2da42aaeeda9c046f563cd5e8fca558aa5e
Bakare Abiola <murphybiola007@gmail.com>
2021-11-19 00:49:27 -0800

    Completed the destination page

--- pages/destination.vue
+++ pages/destination.vue
@@ -2,7 +2,7 @@
   <div
     class="h-full w-full z-10 bg-cover bg-center bg-no-repeat"
     :style="{
-      backgroundImage: 'url(' + require(`~/assets/img/destination/${bgImage}.jpg`) + ')',
+      backgroundImage: 'url(' + require(`~/assets/img/destinations/${bgImage}.jpg`) + ')',
     }"
   >
     <div
@@ -10,36 +10,41 @@
     >
       <div class="flex flex-wrap w-full">
         <div class="lg:w-1/2 text-white text-center">
-          <p class="text-xl pb-2 uppercase lg:text-left">Pick your destination</p>
+          <p class="text-xl pb-2 uppercase lg:text-left">01 Pick your destination</p>
           <img
-            class="h-45 w-45"
-            :src="require(`~/assets/img/destination/${currenTabImage}.webp`)"
+            class="h-45 w-45 p-16"
+            :src="require(`~/assets/img/destinations/${currentTab}`)"
             alt=""
           />
         </div>
         <div class="lg:w-1/2 w-full">
           <nav class="flex flex-row text-white bg-transparent justify-start">
-            <a
-              class="flex items-center font-semibold py-2 mt-2 text-sm bg-transparent min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white md:active:border-white"
-            >
-              MOON
-            </a>
-            <a
-              class="flex items-center font-semibold py-2 mt-2 text-sm bg-transparent min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white"
-            >
-              MOON
-            </a>
-            <a
-              class="flex items-center font-semibold py-2 mt-2 text-sm bg-transparent min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white md:active:border-white"
-            >
-              MOON
-            </a>
-            <a
-              class="flex items-center font-semibold py-2 mt-2 text-sm bg-transparent min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white md:active:border-white"
-            >
-              MOON
-            </a>
+            <template v-for="(destination, index) in destinations">
+              <a
+                class="flex items-center font-semibold py-2 mt-2 text-sm bg-transparent min-h-16 px-4 md:px-0 md:mx-4 md:hover:border-b-2 md:hover:border-white uppercase"
+                @click="handleSelect(index)"
+                :class="{ 'i-tab-active': destination.active }"
+              >
+                {{ destination.name }}
+              </a>
+            </template>
           </nav>
+          <div class="text-white">
+            <p class="text-8xl uppercase py-10 text-left">{{ name }}</p>
+            <p class="text-xl pb-8">{{ description }}</p>
+
+            <hr class="pb-3" />
+            <div class="flex">
+              <div class="w-1/2">
+                <p>AVERAGE. DISTANCE</p>
+                <p class="text-2xl">{{ distance }}</p>
+              </div>
+              <div class="w-1/2">
+                <p>EST. TRAVEL TIME</p>
+                <p class="text-2xl">{{ travel }}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -47,18 +52,19 @@
 </template>
 
 <script>
+import data from "~/content/data.json";
 export default {
   data() {
     return {
       tabActive: ["border-b-2", "border-white"],
-      currenTabImage: "image-moon",
-      images: {
-        moon: "image-moon",
-        europa: "image-eroupa",
-      },
+      currentTab: "image-moon.webp",
+      destinations: data.destinations,
+      description: data.destinations[0].description,
+      name: data.destinations[0].name,
+      distance: data.destinations[0].distance,
+      travel: data.destinations[0].travel,
     };
   },
-
   computed: {
     bgImage() {
       if (this.$store.state.deviceType === "mobile")
@@ -68,11 +74,32 @@ export default {
       else return "background-destination-desktop";
     },
   },
+  methods: {
+    handleSelect(index) {
+      this.destinations.forEach((destination, i) => {
+        destination.active = index == i; //true(active)
+        if (index == i) {
+          this.currentTab = destination.images.webp;
+          this.name = destination.name;
+          this.distance = destination.distance;
+          this.travel = destination.travel;
+          this.description = destination.description;
+        }
+      });
+    },
+  },
+
+  mounted() {
+    //to initially select the first active tab
+    this.$nextTick(function () {
+      this.handleSelect(0);
+    });
+  },
 };
 </script>
 
 <style scoped>
-.tab-active {
+.i-tab-active {
   @apply border-b-2 border-white;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,6 @@
 </template>
 
 <script>
-import iData from "~/content/data.json";
 export default {
   data() {
     return {
